ca6b75cf121c1c815c3d859f237056512c5b8ce8
orcunmadran <orcunmadran@gmail.com>
2015-12-18 16:16:34 +0200

    ilk gönderi

---
+++ database.js
@@ -0,0 +1,23 @@
+var catalog = [
+	{
+        "itemName":"Bisiklet",
+        "itemAuthor":"Paul Fattaruso",
+        "itemImage":"images/bisiklet.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Bisikletin yola karşı zaaflarıyla tanışın. Pedalınızı çevirmeden önce artık iki kere düşüneceksiniz : Gün ışığı, renkler, atmosfer, dakikalar hepsi sizin için birer pusula görevinde olacak, eğer birini unutursanız, emin olun yol da sizi unutur. Bisiklet Kitabı benliğini ve küçük tarihçesini yaratan tekerlekleri biraz daha iyi anlamanıza yardımcı olacak bir rehber niteliğinde.<br>(Tanıtım Bülteninden)</p>"
+    }, 
+	{
+        "itemName":"Bisiklet Öyküleri",
+        "itemAuthor":"Aydın İleri",
+        "itemImage":"images/bisiklet_hikayeleri.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>İnsanın sürerken katılımcı ve öznesi olduğu, kendini her zaman özgür hissettiği, özgürleştiği, çevreye ve insana zarar vermeyen, sürdürülebilir bir hayatın vazgeçilmezidir bisiklet. Hangimizin düşüne girmedi ki o? Üç tekerlekli bisikletle başlayan yolculuğumuz dört tekere, hayran olduğumuz BMX'ten o güzelim Pinokyo'ya, dağ bisikletinden yarış bisikletine uzandı. Zaman ne çabuk geçmiş. İlk bisikletlerimizi, aslında çocukluğumuzu, o eski arkadaşlarımızı, bizi biz yapan mahallemizi, oynadığımız oyunları özlememek mümkün mü?</p><p>Hepimizin kalbinde bir bisiklet yatıyor. Çünkü bisikletimiz hem sırdaşımız hem de yol arkadaşımız oldu hep. Aydın İleri'nin uzun yıllar üzerinde çalışıp özenle hazırladığı bu kitapta onlarca yazar hiç unutamadıkları bisikletlerinin öyküsünü anlattı. Tüm bisiklet tutkunlarının büyük bir keyifle, merakla, bazen de hüzünle okuyacağı yaşanmışlıklar hepimizi geçmişe sürükleyecek. Barış ve doğa için, sağlıklı bir hayat ve özgürlük için dönen tekerler hiç durmasın. Yaşasın bisikletli yaşam!<br>(Tanıtım Bülteninden)</p>"
+    }, 
+	{
+        "itemName":"Bisiklet Zen",
+        "itemAuthor":"Juan Carlos Kreimer",
+        "itemImage":"images/zen.jpg",
+        "itemCreationDate":"2014",
+        "itemDescription":"<p>Bisikletler gündelik yaşamın bir parçası artık. Şu anda çoğunluk, bisikletleri ulaşım aracından ziyade keyifli zaman geçirmek için kullanıyor. Dünyanın hemen hemen tüm şehirlerinde bisikletin yaygınlaşması ve bisikletlinin korunması için caddelerde sınırlar çiziliyor, kendilerine ayrılan yollarla birlikte modası ve etkisi de eş zamanlı olarak çoğalıyor. Biz de sürdürülebilir bir hayat için hakkımız olan keyfi sürmek istiyoruz. Biçim olarak gözüken içeriğin kendisidir ve kullanım amacı ne olursa olsun son sözümüz: Bisiklet alışkanlık yaratır.<br>(Tanıtım Bülteninden)</p>"
+    }
+];
